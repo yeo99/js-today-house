c8c6bd2fe81af20ef7824727f74eaa30f8cf9aef
여승철 <timek70@dalcomsoft.com>
2022-03-21 11:08:26 +0900

    POST방식을 이용하여 글쓰기 기능 구현

--- src/write.js
+++ src/write.js
@@ -1,5 +1,6 @@
 // 제목을 입력받는 .title-input input태그를 가져옴
 const $titleInput = document.querySelector('.title-input');
+const $goBack = document.querySelector('.go-back');
 const $contentInput = document.querySelector('.content-input');
 const $currentTitleLength = document.querySelector('.current-title-length');
 const $publishButton = document.querySelector('.publish-button');
@@ -17,9 +18,37 @@ function checkInputLength({ target }) {
     $currentTitleLength.innerHTML = target.value.length;
 }
 
+
+// db.json에서 입력한 글이 추가된것을 알 수 있음
+async function postSubmit(event) {
+    event.preventDefault(); // 새로고침 방지
+
+    await fetch('http://localhost:1234/posts', {
+        method: 'POST',
+        headers: {
+            // 헤더에 json타입을 넣어준다고 명시.
+            // axios의 경우 자동으로 넣어준다고 함
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            title: $titleInput.value,
+            content: $contentInput.value,
+        })
+    });
+}
+
 // 인풋 입력이 되면 입력값을 체크
 $titleInput.addEventListener('input', checkInputLength);
 
-// 24번째줄 굳이..?
-$postForm.addEventListener('submit', () => console.log("제출!"));
-$publishButton.addEventListener('submit', () => alert('제출!'));
+$postForm.addEventListener('submit', postSubmit);
+$publishButton.addEventListener('click', () => {
+    // eventTarget.dispatchEvent()
+    // eventTarget 인터페이스의 dispatchEvent() 메서드는 eventTarget객체로 Event를 발송
+    // 그리고 해당 이벤트에 대해 등록된 이벤트 리스너들을 (동기적)순서대로 호출
+    $postForm.dispatchEvent(new Event('submit'));
+});
+
+// 뒤로가기 이벤트
+$goBack.addEventListener('click', () => {
+    window.history.back(1);
+})
